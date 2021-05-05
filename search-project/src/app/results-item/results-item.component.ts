12bcd0ce7c7e972ea7b2afa911ac47491d9b2a84
evanesenze <evanesenze@gamil.com>
2021-05-06 03:14:31 +0500

    Ivan_Bazuev

---
+++ search-project/src/app/results-item/results-item.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { SearchElement, WikiResponse } from '../bin/wiki-response';
+
+@Component({
+  selector: 'app-results-item',
+  templateUrl: './results-item.component.html',
+  styleUrls: ['./results-item.component.sass']
+})
+export class ResultsItemComponent implements OnInit {
+  public lastQuery: string | undefined;
+  @Input()
+  public set result(value: WikiResponse | null){
+    this.res = value?.query?.search;
+  }
+  public res: SearchElement[] | undefined;
+
+  constructor() { }
+
+  ngOnInit() { }
+}
