2231a9fd7e14a85f2de271b76baf863a5570dd3d
Foxovsky-bit <mihalov.k@mail.ru>
2021-05-18 18:46:57 +0500

    httpRequests & sortByObject

---
+++ search-project/src/app/services/data.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import { map } from 'rxjs/operators';
+import { CardResult} from "../app.component";
+
+@Injectable()
+export class HttpService{
+
+  constructor(private http: HttpClient){};
+
+  getUserData(query:string):Observable<CardResult[]>{
+    return this.http.get(`https://api.github.com/search/users?q=${query}`).pipe(map((data:any)=>{
+      let resultList = data["items"];
+      return resultList.map(function(card): CardResult {
+        return {name:card.login, img_user:card.avatar_url,url:card.html_url};
+      });
+    }));
+  }
+
+  getRepositoryData(query:string):Observable<CardResult[]>{
+    return this.http.get(`https://api.github.com/search/repositories?q=${query}`).pipe(map((data:any)=>{
+      let resultList = data["items"];
+      return resultList.map(function(card): CardResult {
+        return {name:card.name, img_user:card.owner.avatar_url,url:`https://github.com/${card.full_name}`};
+      });
+    }));
+  }
+}
